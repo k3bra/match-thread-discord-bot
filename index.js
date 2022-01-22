328f54f56ba59f9bd451ab78da774869655cd11f
Eduardo Domingues <eduardo.domingues@hijiffy.com>
2022-01-22 00:28:05 +0000

    Initial commit to make the bot talk to you

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ require('dotenv').config();
 const Discord = require("discord.js");
 const MatchThread = require("./MatchThread");
 const NewspapersFrontPage = require("./NewspapersFrontPage");
+const PredictIntent = require("./PredictIntent");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 const CronJob = require('cron').CronJob;
 
 client.login(process.env.TOKEN)
 
+
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong");
+    if (msg.author.bot === false && msg.content.startsWith("k3brot")) {
+        let predictIntent = new PredictIntent();
+        let message = msg.content.replace("k3brot", "");
+
+        predictIntent.predict(message).then(response => msg.reply(response));
     }
 })
 
